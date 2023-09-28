8840d8049c78b4e9ae9de5e5eca3ac00988ebf60
ethan5432 <ethan.nirenberg@gmail.com>
2023-09-28 17:28:17 -0400

    Finished static feed

--- components/Postfeed.js
+++ components/Postfeed.js
@@ -1,3 +1,4 @@
+import Tweet from "./Tweet";
 import TweetInput from "./TweetInput";
 
 export default function Postfeed() {
@@ -14,7 +15,7 @@ export default function Postfeed() {
         Home
           </div>
           <TweetInput />
-      Feed
+          <Tweet/>
     </div>
   );
 }
--- components/Tweet.js
+++ components/Tweet.js
@@ -1,16 +1,32 @@
+import { ChartBarIcon, ChatIcon, HeartIcon, UploadIcon } from "@heroicons/react/outline"
+
 export default function Tweet() {
     return (
-        <div>
-            <TweetHeader/>
+      <div className="border-b  border-gray-700">
+        <TweetHeader />
+        <div className="p-3 ml-16 text-gray-500 flex space-x-14">
+          <ChatIcon className="w-5 cursor-pointer hover:text-green-400" />
+          <HeartIcon className="w-5 cursor-pointer hover:text-pink-400" />
+          <ChartBarIcon className="w-5 cursor-not-allowed hover:text-green-400" />
+          <UploadIcon className="w-5 cursor-not-allowed hover:text-green-400" />
         </div>
-    )
+      </div>
+    );
 }
 
-export default function TweetHeader() {
+export function TweetHeader() {
     return (
-        <div>
-            <img src="/assets/kylie.png" />
-            <div></div>
+        <div className="flex space-x-3 p-3 ">
+            <img src="/assets/kylie.png" className="w-11 h-11 
+            rounded-full object-cover" />
+            <div>
+                <div className="text-gray-500 flex items-center space-x-2 mb-1">
+                    <span>@kylie</span>
+                    <div className="w-1 h-1 border border-gray-500 rounded-full"></div>
+                    <span>2 hours ago</span>
+                </div>
+                <span>Text</span>
+            </div>
         </div>
     )
 }
