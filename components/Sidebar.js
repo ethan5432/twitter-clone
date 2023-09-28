4440966c589b1bc441a4370c89d2475fe48521ee
ethan5432 <ethan.nirenberg@gmail.com>
2023-09-28 16:01:44 -0400

    Finished nav bar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,27 +1,46 @@
-import {HomeIcon} from "@heroicons/react/outline"
+import {
+    HomeIcon,
+    HashtagIcon,
+    InboxIcon,
+    BookmarkIcon,
+    ClipboardListIcon,
+    BellIcon,
+    UserIcon,
+DotsCircleHorizontalIcon
+} from "@heroicons/react/outline"
+import Image from "next/image"
+
 
 
 export default function Sidebar() {
     return (
-      <div className="hidden sm:flex flex-col fixed">
-        <div>Logo</div>
-        <nav>
-                <SidebarLink Icon={ HomeIcon } text={"Home"} />
-          {/* <SidebarLink text={"Explore"} />
-          <SidebarLink text={"Notifications"} />
-          <SidebarLink text={"Messeges"} /> */}
-
-            </nav>
-            <div>User</div>
+      <div className="h-full hidden sm:flex flex-col fixed xl:ml-24">
+        <nav className="h-full relative xl:space-y-1.5">
+          <div className="flex justify-center py-3 xl:p-3">
+            <Image src={"/assets/twitter-logo.png"} width={34} height={34} />
+          </div>
+          <SidebarLink Icon={HomeIcon} text={"Home"} />
+          <SidebarLink Icon={HashtagIcon} text={"Explore"} />
+          <SidebarLink Icon={BellIcon} text={"Notifications"} />
+          <SidebarLink Icon={InboxIcon} text={"Messeges"} />
+          <SidebarLink Icon={BookmarkIcon} text={"Bookmarks"} />
+          <SidebarLink Icon={UserIcon} text={"Profile"} />
+                <SidebarLink Icon={DotsCircleHorizontalIcon} text={"More"} />
+                <button className="hidden xl:inline bg-[#1d9bf0] 
+                rounded-full h-[52px] mt-2 w-[200px] text-lg font-bold">
+                    Tweet
+                </button>
+          <div className="absolute bottom-0">User</div>
+        </nav>
       </div>
     );
 }
 
 function SidebarLink({text, Icon}) {
     return (
-      <li className="flex mb-6 items-center text-xl space-x-3">
+      <li className="hoverAnimation flex mb-3 items-center text-xl space-x-3 xl:justify-start justify-center">
         <Icon className="h-7" />
-        <span className=" xl:inline">{text}</span>
+        <span className="hidden xl:inline">{text}</span>
       </li>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Sidebar from '@/components/Sidebar'
+import Postfeed from '@/components/Postfeed'
+import Trending from '@/components/Trending'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className="bg-black min-h-screen text-[#E7E9EA]
-    max-w-[1400px] mx-auto">
+    max-w-[1400px] mx-auto flex">
       <Sidebar />
-      {/* <PostFeed /> */}
-      {/* <Trending /> */}
+      <Postfeed />
+      <Trending />
     </div>
   )
 }
