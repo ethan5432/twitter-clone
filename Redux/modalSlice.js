20edf25682875f8b9df2a09138d5d635e25eea00
ethan5432 <ethan.nirenberg@gmail.com>
2023-10-03 17:08:19 -0400

    creating modal slices

---
+++ Redux/modalSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const modalSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = modalSlice.actions
+
+export default modalSlice.reducer
